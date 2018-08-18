3ae6dd8343044eeaea4567049cfb7e94123c0b2c
Unknown <karinaaakos@gmail.com>
2018-08-18 17:13:05 +0200

    Added bootstrap

--- src/search.js
+++ src/search.js
@@ -1,7 +1,6 @@
+import 
+
 class Search {
     constructor() { }
        };
-       this.szukaj = 'wprowadz tekst';
-    start = url => {
-        document.write('Zaczynamy') };
   export default Search;
