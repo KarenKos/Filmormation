e9625ec1c7e78213dcc766214a44c7db935f9a39
Osakr <dyngusoskar@gmail.com>
2018-08-23 14:01:28 +0200

    ify gdy brak jakiegoś elementu

--- src/movie.js
+++ src/movie.js
@@ -1,32 +1,125 @@
+import More from "./more";
+import Rating from "./rating";
+
 class Movie 
 {
-    constructor(resp) 
-    {
+    constructor(resp) {
         this._resp = resp;
         this._film = document.querySelector("#film");
+        const submit2 = document.querySelector("#submit2");
+        const More = new More();
     }
-    renderResponse(resp)
-    {
-        //Wyświetlanie informacji o błędzie
-        if (!resp) 
-        {
-            console.log(resp.status);
-        }
-        //tworzenie zmiennych, w których będą przechowane informacje o filmie
+    displayMore(event) {
+        event.preventDefault();
+        More.method();
+    }
+    renderResponse(resp) {
         const title = resp.Title;
         const plot = resp.Plot;
         const year = resp.Released;
         const poster = resp.Poster;
-
-        //Tworzenie karty z informacjami o filmie na podstawie bootstrap
-        this._film.innerHTML = `<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${poster}/100px180/" alt="Film image">
-        <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${year}</h6>
-        <p class="card-text">${plot}</p>
-        </div>
-        </div>`;
+        if (title === undefined) {
+            alert("Nie znaleziona filmu.\nSpróbuj użyć wyszukiwania zaawansowanego.");
+        }
+        else if (!resp) {
+            console.log(resp.status);
+        }
+        else if (poster === "N/A") {
+            if (plot === "N/A" & year === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">date of release is undefined</h6>
+                <p class="card-text">Plot is undefined</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else if (plot === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${year}</h6>
+                <p class="card-text">Plot is undefined</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else if (year === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">date of release is undefined</h6>
+                <p class="card-text">${plot}</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${year}</h6>
+                <p class="card-text">${plot}</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+        }
+        else {
+            if (plot === "N/A" & year === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${poster}/100px180/" alt="Film image">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">date of release is undefined</h6>
+                <p class="card-text">Plot is undefined</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else if (plot === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${poster}/100px180/" alt="Film image">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${year}</h6>
+                <p class="card-text">Plot is undefined</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else if (year === "N/A") {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${poster}/100px180/" alt="Film image">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">date of release is undefined</h6>
+                <p class="card-text">${plot}</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+            else {
+                this._film.innerHTML += `<div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${poster}/100px180/" alt="Film image">
+                <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${year}</h6>
+                <p class="card-text">${plot}</p>
+                <a href="#" id="submit2" class="btn btn-primary">More</a>
+                </div>
+                </div>`;
+                submit2.addEventListener("click", this.displayMore);
+            }
+        }
         return;
     }
 }
